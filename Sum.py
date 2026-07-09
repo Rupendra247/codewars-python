efd70c43d607df9af301bd96462f0979c80bebef
Rupendra Dhungana <dhunganarupendra03@gmail.com>
2026-07-09 11:21:12 +0545

    changes the file name

---
+++ Sum.py
@@ -0,0 +1,11 @@
+## so sum of the given number
+
+
+def sum_array(a):     
+        if a == None :
+            return 0
+        else:
+            return sum(a)     
+        
+a = sum_array([1,2,3])
+print(a)
--- def grow(arr):      product = 1     for i in arr:         product *= i     return product.py
+++
@@ -1,5 +0,0 @@
-def sum_array(a):     
-        if a == None :
-            return 0
-        else:
-            return sum(a)     
